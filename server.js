6f1b9043ba6148e0fa52d02f76318f88f40d1cfa
vinwik <vinwik@hotmail.com>
2020-04-04 03:08:43 +0100

    try fix static routing

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use("/", express.static("client/build"));
-  app.get("*", (res, req) => {
+  app.use(express.static("client/build"));
+  app.get("/*", (res, req) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
